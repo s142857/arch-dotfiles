983c9881f990064b14e79d140b49939c8e3b6776
Sanketh Menda <mendasanketh@gmail.com>
2017-03-18 14:04:07 -0400

    Back to minimalistic emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,6 @@
-;; Transparency
 
-;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
-;;(set-frame-parameter (selected-frame) 'alpha <both>)
-(set-frame-parameter (selected-frame) 'alpha '(95 . 50))
-(add-to-list 'default-frame-alist '(alpha . (95 . 50)))
+;;;;  Package Management  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;disable toolbar and menubar
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-
-;; Package Management
 (require 'package)
 (add-to-list 'package-archives '("org" , "http://orgmode.org/elpa/"))
 ;;(add-to-list 'package-archives '("melpa-stable" , "http://stable.melpa.org/packages/"))
@@ -19,15 +10,25 @@
 
 (package-initialize)
 
-;;Fontg
+
+;;;;  appearance  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; disable toolbar and menubar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; Font
 (set-face-attribute 'default nil
                     :family "Inconsolata" ; Inconsolata is ♥
-                    :height 120
+                    :height 110
                     :weight 'normal
                     :width 'normal)
 
-;;Tex
-(require 'cl)
+;; Themes
+(if (display-graphic-p) 
+    (load-theme 'dracula t)
+  (load-theme 'zenburn))
+(setq custom-safe-themes t)
 
 ;;Powerline
 (require 'powerline)
@@ -39,56 +40,9 @@
 ;; Visual Line mode
 (global-visual-line-mode 1)
 
-;;Racket Mode
-(require 'racket-mode)
 
-;;rainbow delimiters
-(add-hook 'racket-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'racket-repl-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'elpy-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(define-key racket-mode-map (kbd "C-c C-c") 'racket-test)
-(define-key racket-mode-map (kbd "C-c C-r") 'racket-run)
+;;;;;  smex  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;org-mode
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-
-;; Colour terminals
-(setq ansi-color-for-comint-mode t)
-;; python
-(setq python-shell-interpreter "ipython"
-    python-shell-interpreter-args "--simple-prompt -i")
-
-;; Haskell
-(add-hook 'haskell-mode-hook #'hindent-mode)
-
-;; C++ development
-(require 'rtags) ;; optional, must have rtags installed
-(cmake-ide-setup)
-
-;; Themes
-(if (display-graphic-p) 
-    (load-theme 'dracula t)
-  (load-theme 'zenburn))
-(setq custom-safe-themes t)
-
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/blackboard-theme.el"); My third favourite theme
-
-;;enable python elpy
-(elpy-enable)
-
-;; smartparens
-(require 'smartparens-config)
-(smartparens-global-mode t)
-
-;;(add-hook 'python-mode-hook 'anaconda-mode)  ;;more awesomeness
-;;(add-hook 'python-mode-hook 'anaconda-eldoc-mode)  ;;docs
-
-;; smex
 (require 'smex)
 (smex-initialize)
 ;; smex keybinding
@@ -110,16 +64,27 @@
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 
+
+;;;;;  TeX  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+
+;;Tex
+(require 'cl)
+
 ;; Latex
 (require 'latex)
 (add-hook 'LaTeX-mode-hook #'flyspell-mode)
 (add-hook 'LaTeX-mode-hook #'auto-complete-mode)
 (add-hook 'LaTeX-mode-hook #'latex-math-mode)
+(setq TeX-save-query nil)
+(define-key LaTeX-mode-map (kbd "S-<return>") 'TeX-command-run-all)
 
 ;; Unbinding C-z
 (global-unset-key [(control z)])
 (global-unset-key [(control x)(control z)])
 (global-set-key [(control z)] 'undo)
+
+;; Sloppy hands!
+(global-unset-key [(control x)(control c)])
 
 ;; Latex shortcuts
 (define-key LaTeX-mode-map (kbd "C-c f") 'LaTeX-math-frac)
@@ -130,31 +95,19 @@
 (define-key LaTeX-mode-map (kbd "C-c i") 'LaTeX-math-implies)
 (define-key LaTeX-mode-map (kbd "C-c a") 'LaTeX-math-forall)
 (define-key LaTeX-mode-map (kbd "C-c e") 'LaTeX-math-exists)
-(define-key LaTeX-mode-map (kbd "C-c 2") "^{2}")
-(define-key LaTeX-mode-map (kbd "C-c 3") "^{3}")
+(define-key LaTeX-mode-map (kbd "` 2") "^{2}")
+(define-key LaTeX-mode-map (kbd "` 3") "^{3}")
+(define-key LaTeX-mode-map (kbd "` t") "\dagger")
 (define-key LaTeX-mode-map (kbd "C-c d") "\\frac{dy}{dx}")
-(define-key LaTeX-mode-map (kbd "C-c m") "\\lim_{x \\rightarrow a}")
+(define-key LaTeX-mode-map (kbd "C-c m") "\\lim_{n \\rightarrow \infty}")
 (define-key LaTeX-mode-map (kbd "C-c h") "\\lim_{h \\rightarrow 0}")
 (define-key LaTeX-mode-map (kbd "C-c (") "\\left(")
 (define-key LaTeX-mode-map (kbd "C-c 0") "_{0}")
 (define-key LaTeX-mode-map (kbd "C-c q") "\\hfill \\ensuremath \\square")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("f641bdb1b534a06baa5e05ffdb5039fb265fde2764fbfd9a90b0d23b75f3936b" "427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "345f8f92edc3508574c61850b98a2e0a7a3f5ba3bb9ed03a50f6e41546fe2de0" default)))
- '(org-support-shift-select t)
- '(package-selected-packages
-   (quote
-    (python-docstring java-imports dracula blackboard-bold-mode python-django twittering-mode soundcloud auctex-latexmk auto-complete-auctex auto-complete latex-extra zenburn-theme cmake-ide rtags ggtags rainbow-delimiters smex smartparens racket-mode powerline haskell-mode elpy dracula-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(racket-selfeval-face ((t (:foreground "medium spring green")))))
-(put 'erase-buffer 'disabled nil)
+
+;;;;;  General Programming  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; smartparens
+(require 'smartparens-config)
+(smartparens-global-mode t)
